df5c478c50f1d6ecec5d91cd6f9eca97b46d43a8
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-02-16 15:31:26 +0600

    Added UINavigationController override function

--- Disable Rotation/ViewController.swift
+++ Disable Rotation/ViewController.swift
@@ -26,8 +26,52 @@ class ViewController: UIViewController {
         
     }
     
+    override var shouldAutorotate: Bool{
+        
+        return false
+    }
     
 
 
+}
+
+extension UINavigationController{
+    
+    open override var shouldAutorotate: Bool{
+        
+        get {
+            
+            if let visibleVC = visibleViewController{
+                return visibleVC.shouldAutorotate
+            }
+            return super.shouldAutorotate
+        }
+    }
+    
+    open override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation{
+        
+        get{
+            
+            if let visibleVC = visibleViewController{
+                
+                return visibleVC.preferredInterfaceOrientationForPresentation
+            }
+            return super.preferredInterfaceOrientationForPresentation
+        }
+    }
+    
+    open override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        
+        get {
+            
+            
+            if let visibleVC = visibleViewController{
+                
+                return visibleVC.supportedInterfaceOrientations
+            }
+            return super.supportedInterfaceOrientations
+        }
+    }
+    
 }
 
